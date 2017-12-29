3083fe96693f99950197ce29d662ac1b9d4dbacb
Ileana <argyris.ileana@gmail.com>
2017-12-29 01:50:01 -1000

    Initial commit

---
+++ 30DaysOfCode/day1DataTypes.py
@@ -0,0 +1,18 @@
+import sys
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+i2=int(sys.stdin.readline())
+d2=float(sys.stdin.readline())
+s2=sys.stdin.readline()
+
+# Print the sum of both integer variables on a new line.
+print i + i2
+
+# Print the sum of the double variables on a new line.
+print d + d2
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + s2
