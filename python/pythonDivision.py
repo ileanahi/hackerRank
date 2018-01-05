ae0bfcd858d4a110f7a4f91d7e65c4a9e4426310
Ileana <argyris.ileana@gmail.com>
2018-01-04 23:04:35 -1000

    Update pythonDivision.py

--- python/pythonDivision.py
+++ python/pythonDivision.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from __future__ import division
 if __name__ == '__main__':
     a = int(raw_input())
@@ -6,12 +5,3 @@ if __name__ == '__main__':
 
     print a//b
     print a/b
-=======
-from __future__ import division
-if __name__ == '__main__':
-    a = int(raw_input())
-    b = int(raw_input())
-
-    print a//b
-    print a/b
->>>>>>> e915652e0005753e5b2899f7b5b9b443be34aacb
