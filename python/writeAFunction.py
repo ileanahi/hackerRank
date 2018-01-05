7e9681907aa92b410f8031d3c2784bf63fcaa391
Ileana <argyris.ileana@gmail.com>
2018-01-04 23:05:31 -1000

    Update writeAFunction.py

--- python/writeAFunction.py
+++ python/writeAFunction.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def is_leap(year):
     leap = False
 
@@ -6,12 +5,3 @@ def is_leap(year):
     return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
 year = int(raw_input())
 print is_leap(year)
-=======
-def is_leap(year):
-    leap = False
-
-    # Write your logic here
-    return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
-year = int(raw_input())
-print is_leap(year)
->>>>>>> e915652e0005753e5b2899f7b5b9b443be34aacb
