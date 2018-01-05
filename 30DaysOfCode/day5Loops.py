2b36ae1182ae71aaf0f57a19860e7d7a6b61dea3
Ileana <argyris.ileana@gmail.com>
2018-01-04 23:01:01 -1000

    Update day5Loops.py

--- 30DaysOfCode/day5Loops.py
+++ 30DaysOfCode/day5Loops.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/python
 
 import sys
@@ -8,14 +7,3 @@ n = int(raw_input().strip())
 for i in range(1,11):
     print "{} x {} =".format(n,i), n * i
     i+=1
-=======
-#!/bin/python
-
-import sys
-
-n = int(raw_input().strip())
-
-for i in range(1,11):
-    print "{} x {} =".format(n,i), n * i
-    i+=1
->>>>>>> e915652e0005753e5b2899f7b5b9b443be34aacb
