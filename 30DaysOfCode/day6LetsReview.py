47d84393492df1a2d3060c36872825b5f24a0cbc
Ileana <argyris.ileana@gmail.com>
2018-01-04 23:01:23 -1000

    Update day6LetsReview.py

--- 30DaysOfCode/day6LetsReview.py
+++ 30DaysOfCode/day6LetsReview.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 for i in range(int(input())): s=input(); print(*["".join(s[::2]),"".join(s[1::2])])
-=======
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-for i in range(int(input())): s=input(); print(*["".join(s[::2]),"".join(s[1::2])])
->>>>>>> e915652e0005753e5b2899f7b5b9b443be34aacb
