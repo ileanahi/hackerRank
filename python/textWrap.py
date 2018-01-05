b4fa66bb8332f165497f7e4e06d3fd78108232a4
Ileana <argyris.ileana@gmail.com>
2018-01-04 23:05:14 -1000

    Update textWrap.py

--- python/textWrap.py
+++ python/textWrap.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 def wrap(string, max_width):
     return textwrap.fill(string, max_width)
-=======
-def wrap(string, max_width):
-    return textwrap.fill(string, max_width)
->>>>>>> e915652e0005753e5b2899f7b5b9b443be34aacb
