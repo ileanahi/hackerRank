717edec143e9e07cbe3ab0c662552e7a9bc75356
Ileana <argyris.ileana@gmail.com>
2018-01-04 23:04:58 -1000

    Update stringValidators.py

--- python/stringValidators.py
+++ python/stringValidators.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 if __name__ == '__main__':
     s = raw_input()
     alnumRes = False
@@ -24,30 +23,3 @@ if __name__ == '__main__':
     print digitRes
     print lowerRes
     print upperRes
-=======
-if __name__ == '__main__':
-    s = raw_input()
-    alnumRes = False
-    alphaRes = False
-    digitRes = False
-    lowerRes = False
-    upperRes = False
-
-    for i in s:
-        if i.isalnum() == True:
-            alnumRes = True
-        if i.isalpha() == True:
-            alphaRes = True
-        if i.isdigit() == True:
-            digitRes = True
-        if i.islower() == True:
-            lowerRes = True
-        if i.isupper() == True:
-            upperRes = True
-
-    print alnumRes
-    print alphaRes
-    print digitRes
-    print lowerRes
-    print upperRes
->>>>>>> e915652e0005753e5b2899f7b5b9b443be34aacb
