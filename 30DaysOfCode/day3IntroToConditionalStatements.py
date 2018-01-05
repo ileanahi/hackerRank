b87e9a1820cd7c25e42cae182c4eea532000f5c6
Ileana <argyris.ileana@gmail.com>
2018-01-04 23:00:04 -1000

    Update day3IntroToConditionalStatements.py

--- 30DaysOfCode/day3IntroToConditionalStatements.py
+++ 30DaysOfCode/day3IntroToConditionalStatements.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/python
 
 import sys
@@ -12,18 +11,3 @@ elif N % 2 == 0 and N > 20:
     print "Not Weird"
 else:
     print "Weird"
-=======
-#!/bin/python
-
-import sys
-N = int(raw_input().strip())
-
-if N % 2 == 0 and N >= 2 and N <= 5:
-    print "Not Weird"
-elif N % 2 == 0 and N >= 6 and N <= 20:
-    print "Weird"
-elif N % 2 == 0 and N > 20:
-    print "Not Weird"
-else:
-    print "Weird"
->>>>>>> e915652e0005753e5b2899f7b5b9b443be34aacb
