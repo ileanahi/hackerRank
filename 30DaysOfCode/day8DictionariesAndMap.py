b34c470ba07c94c46fd42a8cc6ca4b605e5c4bfc
Ileana <argyris.ileana@gmail.com>
2018-01-04 23:03:11 -1000

    Update day8DictionariesAndMap.py

--- 30DaysOfCode/day8DictionariesAndMap.py
+++ 30DaysOfCode/day8DictionariesAndMap.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 from sys import stdin
 
@@ -9,15 +8,3 @@ if __name__ == "__main__":
     phoneBook = dict(i.split() for i in lines[1:n+1])
 
     print( "\n".join(["{0:}={1:}".format(i, phoneBook[i]) if i in phoneBook else "Not found" for i in lines[n+1:] ]) )
-=======
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-from sys import stdin
-
-if __name__ == "__main__":
-    lines = stdin.read().splitlines()
-
-    n = int(lines[0])
-    phoneBook = dict(i.split() for i in lines[1:n+1])
-
-    print( "\n".join(["{0:}={1:}".format(i, phoneBook[i]) if i in phoneBook else "Not found" for i in lines[n+1:] ]) )
->>>>>>> e915652e0005753e5b2899f7b5b9b443be34aacb
