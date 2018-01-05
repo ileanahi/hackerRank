3f26efa68842c0897365a451c72e7e1614104b47
Ileana <argyris.ileana@gmail.com>
2018-01-04 23:02:00 -1000

    Update day7Arrays.py

--- 30DaysOfCode/day7Arrays.py
+++ 30DaysOfCode/day7Arrays.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/python
 
 import sys
@@ -9,15 +8,3 @@ arr = map(int,raw_input().strip().split(' '))
 for i in range(1,len(arr)+1):
     print arr[len(arr)-i],
     i+=1
-=======
-#!/bin/python
-
-import sys
-
-n = int(raw_input().strip())
-arr = map(int,raw_input().strip().split(' '))
-
-for i in range(1,len(arr)+1):
-    print arr[len(arr)-i],
-    i+=1
->>>>>>> e915652e0005753e5b2899f7b5b9b443be34aacb
